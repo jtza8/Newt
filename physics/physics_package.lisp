3809efd2b928017d061a71f09569a79ad4119537
Jens Thiede <jens@echo.local>
2009-11-26 15:13:13 +0200

    Exporting of symbols of NEWT-PHYSICS.

--- physics/physics_package.lisp
+++ physics/physics_package.lisp
@@ -1,2 +1,5 @@
 (defpackage :newt-physics
-  (:use common-lisp))
+  (:use common-lisp)
+  (:export :presence :x :y :points :axes :project-onto-axis :collides-with
+           :unit-vector :uvector :point :distance :almost-equal
+           :equal-to-n))
