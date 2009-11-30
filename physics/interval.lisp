9caefa60a8297e3a409126c2a75cace3a4458ee1
Jens Thiede <jens@echo.local>
2009-11-30 12:53:46 +0200

    Basic interval class complete.

--- physics/interval.lisp
+++ physics/interval.lisp
@@ -5,3 +5,54 @@
 
 (in-package :newt-physics)
 
+(defclass interval ()
+  ((lower :initarg :lower
+          :initform (error "No lower bound specified.")
+          :reader lower)
+   (upper :initarg :upper
+          :initform (error "No upper bound specified.")
+          :reader upper)
+   (lower-inclusive :initarg :lower-inclusive
+                    :initform t
+                    :reader lower-inclusive)
+   (upper-inclusive :initarg :upper-inclusive
+                    :initform t
+                    :reader upper-inclusive)))
+
+(defun interval (lower upper &key (lower-inclusive t) (upper-inclusive t))
+  (when (> lower upper) (error "lower bound more than upper bound"))
+  (make-instance 'interval
+                 :lower lower
+                 :upper upper
+                 :lower-inclusive lower-inclusive
+                 :upper-inclusive upper-inclusive))
+
+(defmethod expand ((interval interval) numbers
+                   &key (lower-inclusive t) (upper-inclusive t))
+  (let ((lower (lower interval))
+        (upper (upper interval)))
+    (dolist (number numbers (interval lower upper
+                                      :lower-inclusive lower-inclusive
+                                      :upper-inclusive upper-inclusive))
+      (cond ((< number lower) (setf lower number))
+            ((> number upper) (setf upper number))))))
+
+(defmethod overlaps ((interval-a interval) (interval-b interval))
+  (or (and (< (lower interval-a) (upper interval-b))
+           (> (upper interval-a) (lower interval-b)))
+      (and (and (lower-inclusive interval-a) (upper-inclusive interval-b))
+           (= (lower interval-a) (upper interval-b)))
+      (and (and (upper-inclusive interval-a) (lower-inclusive interval-b))
+           (= (upper interval-a) (lower interval-b)))))
+
+(defmethod within ((interval-a interval) (interval-b interval))
+  (or (and (or (> (lower interval-a) (lower interval-b))
+               (and (or (lower-inclusive interval-b)
+                        (and (not (lower-inclusive interval-b))
+                             (not (lower-inclusive interval-a))))
+                    (= (lower interval-a) (lower interval-b))))
+           (or (< (upper interval-a) (upper interval-b))
+               (and (or (upper-inclusive interval-b)
+                        (and (not (upper-inclusive interval-b))
+                             (not (upper-inclusive interval-a))))
+                    (= (upper interval-a) (upper interval-b)))))))
--- physics/tests/interval_test.lisp
+++ physics/tests/interval_test.lisp
@@ -7,3 +7,45 @@
 
 (defclass interval-test (test-case)
   ())
+
+(def-test-method test-expand ((test interval-test))
+  (let ((interval (interval 3 5)))
+    (assert-equal 3 (lower interval))
+    (assert-equal 5 (upper interval))
+    (setf interval (expand interval '(8 -1)))
+    (assert-equal -1 (lower interval))
+    (assert-equal 8 (upper interval))))
+
+(def-test-method test-overlaps ((test interval-test))
+  (let ((interval-a (interval -100 100))
+        (interval-b (interval 50 200))
+        (interval-c (interval 30 50 :upper-inclusive nil))
+        (interval-d (interval 100 520 :lower-inclusive nil))
+        (interval-e (interval -10 50))
+        (interval-f (interval 200 700)))
+    (assert-true (overlaps interval-a interval-b))
+    (assert-true (overlaps interval-b interval-a))
+    (assert-false (overlaps interval-b interval-c))
+    (assert-false (overlaps interval-a interval-d))
+    (assert-true (overlaps interval-b interval-e))
+    (assert-true (overlaps interval-b interval-f))))
+
+(def-test-method test-within ((test interval-test))
+  (let ((interval-a (interval 3 7))
+        (interval-b (interval -100 100))
+        (interval-c (interval 95 123))
+        (interval-d (interval -123 -95))
+        (interval-e (interval 7 123
+                              :lower-inclusive nil
+                              :upper-inclusive nil))
+        (interval-f (interval -100 100
+                              :lower-inclusive nil
+                              :upper-inclusive nil)))
+    (assert-true (within interval-a interval-b))
+    (assert-true (within interval-b interval-b))
+    (assert-false (within interval-b interval-a))
+    (assert-false (within interval-c interval-b))
+    (assert-false (within interval-d interval-b))
+    (assert-false (within interval-a interval-e))
+    (assert-true (within interval-e interval-e))
+    (assert-true (within interval-f interval-b))))
